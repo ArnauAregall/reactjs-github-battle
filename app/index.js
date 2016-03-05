90053a71a82002f4b2f08a9a5c34791b6be5da28
Arnau Aregall <arnau@ar3soft.com>
2016-03-05 10:14:32 +0100

    Pure function components example. Also proves that with webpack can require any module from your app .js files! :)

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var React = require('react'),
-	ReactDOM = require('react-dom');
+	ReactDOM = require('react-dom'),
+	md5 = require('md5');
 
 var HelloWorld = React.createClass({
 	render: () => {
@@ -11,4 +12,27 @@ var HelloWorld = React.createClass({
 	}
 });
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+// Pure functions example
+var ProfilePic = (props) => {
+	return <img className="thumbnail" src={'http://www.gravatar.com/avatar/' + md5(props.email)} />
+}
+
+var ProfileLink = (props) => {
+	return (
+		<a href={'http://www.gravatar.com/' + md5(props.email)}>
+			{props.email}
+		</a>
+	)
+}
+
+var Avatar = (props) => {
+	return (
+		<div className="well">
+			<HelloWorld />
+			<ProfilePic email={props.email} />
+			<ProfileLink email={props.email} />
+		</div>
+	)
+}
+
+ReactDOM.render(<Avatar email="arnau@ar3soft.com" />, document.getElementById('app'));
